674af915c00f3aca1412557dfcccaef2c6798bbe
Shronik Thakur <shronik.thakur@tcs.com>
2016-12-09 11:06:26 +0530

    Add user micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
 	def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -13,19 +14,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-<<<<<<< HEAD
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-=======
        @user.send_activation_email
       # Handle a successful save.
        flash[:info] = "Please check your email to activate your account."
        redirect_to root_url
->>>>>>> account-activation
     else
       render 'new'
     end
+    
   end
 
   def edit
@@ -59,17 +58,16 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-      	store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    # Before filters
 
+    # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -22,54 +23,68 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
+    
   end
+
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
+  def authenticated?(remember_token)
+    digest = send(":remember_token")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
+  end
 
   def activate
     update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
   end
+
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
   def authenticated?(remember_token)
   	return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
-
   end
+
   def forget
     update_attribute(:remember_digest, nil)
   end
+
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
+
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+ 
   private
-
-    # Converts email to all lower-case.
+     # Converts email to all lower-case.
     def downcase_email
-      self.email = email.downcase
+      self.email = self.email.downcase
     end
-
+    
     # Creates and assigns the activation token and digest.
     def create_activation_digest
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
-  end
+
 end
